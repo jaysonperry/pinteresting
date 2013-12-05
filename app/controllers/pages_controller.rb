328c2ebabd79e94b95eba2aac70deec472217370
Jayson Perry <jayson.t.perry@gmail.com>
2013-12-05 14:45:30 -0800

    Bootstrap First Changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,8 @@ class PagesController < ApplicationController
   end
   def about
   end
+  def signup
+  end
+  def signin
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Pinteresting::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
+  get "signup" => "pages#signup"
+  get "signin" => "pages#signin"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
